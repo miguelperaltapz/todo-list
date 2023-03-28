c89c8794e645ed4e72af22a2cd4985936d18a076
miguelperaltapz <miguelperaltapz@gmail.com>
2023-03-28 15:15:59 -0600

    Add Items Functionality and Styles

--- app.js
+++ app.js
@@ -3,40 +3,29 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
+let items = [];
+
 app.set('view engine', 'ejs');
 
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
 app.get("/", function(req, res) {
-    var today = new Date();
-    var currentDay = today.getDay();
-    var day = "";
-
-    switch(currentDay) {
-        case 0:
-            day = "Sunday";
-            break;
-        case 1:
-            day = "Monday";
-            break;
-        case 2:
-            day = "Tuesday";
-            break;
-        case 3:
-            day = "Wednesday";
-            break;
-        case 4:
-            day = "Thursday";
-            break;
-        case 5:
-            day = "Friday";
-            break;
-        case 6:
-            day = "Saturday";
-            break;
-        default:
-            console.log("Error: current day is equal to " + currentDay);
+    let today = new Date();
+    let options = {
+        weekday: "long",
+        day: "numeric",
+        month: "long"
     }
+    let day = today.toLocaleDateString("en-US", options);
 
-    res.render("list", {day: day});
+    res.render("list", {kindOfDay: day, newListItems: items});
+});
+
+app.post("/", function(req, res) {
+    let item = req.body.newItem;
+    items.push(item);
+    res.redirect("/");
 });
 
 app.listen(3000, function() {
